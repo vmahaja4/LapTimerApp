6c54c580823cc5f6748889a363f192c6e7fc0cb5
Vaishh09 <vaishnavi.mahajan3017@gmail.com>
2025-09-12 09:27:37 -0700

    Initial Commit

---
+++ LapTimerApp/LapTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LapTimerAppApp.swift
+//  LapTimerApp
+//
+//  Created by Vaishnavi Mahajan on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LapTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LapTimerAppTests/LapTimerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LapTimerAppTests.swift
+//  LapTimerAppTests
+//
+//  Created by Vaishnavi Mahajan on 9/12/25.
+//
+
+import Testing
+@testable import LapTimerApp
+
+struct LapTimerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
